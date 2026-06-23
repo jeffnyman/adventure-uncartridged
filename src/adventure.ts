3ec82491682c7a37e86d5f7a626c97f3c98aeba6
Jeff Nyman <jeffnyman@gmail.com>
2026-06-23 02:57:28 -0500

    chore: refine state machine logic

--- src/adventure.ts
+++ src/adventure.ts
@@ -12,20 +12,52 @@ export function startGame(): void {
   const select = readSelectSwitch();
 
   if (gameState !== GameState.Win && switchReset && !reset) {
-    // handle a reset action
+    tickResetState();
   } else if (gameState === GameState.GameSelect) {
-    // handle a select action
+    tickSelectState(select);
   } else if (isGameActive()) {
-    // handle game actions
+    tickActiveGameState(select);
   } else if (gameState === GameState.Win) {
-    TickWinState(reset, select);
+    tickWinState(reset, select);
   }
 
   switchReset = reset;
   switchSelect = select;
 }
 
-function TickWinState(reset: boolean, select: boolean): void {
+function tickActiveGameState(select: boolean): void {
+  if (switchSelect && !select) {
+    // Select was released mid-game.
+    gameState = GameState.GameSelect;
+  } else {
+    // Core level logic.
+    if (gameState === GameState.Active1) {
+      // handle level 1
+    } else if (gameState === GameState.Active2) {
+      // handle level 2
+    } else if (gameState === GameState.Active3) {
+      // handle level 3
+    }
+  }
+}
+
+function tickResetState(): void {
+  if (gameState === GameState.GameSelect) {
+    // Re-initialize level. Full game reset.
+  } else {
+    // Mid-game reset.
+  }
+
+  gameState = GameState.Active1;
+}
+
+function tickSelectState(select: boolean): void {
+  if (switchSelect && !select) {
+    // handle level selection
+  }
+}
+
+function tickWinState(reset: boolean, select: boolean): void {
   // Either switch released exits the win state and goes back to
   // the level selection.
   if ((switchReset && !reset) || (switchSelect && !select)) {
