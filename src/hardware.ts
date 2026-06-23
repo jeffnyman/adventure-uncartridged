ddbd71f4e8cc39ff514d39d83c6a05753ddc7be3
Jeff Nyman <jeffnyman@gmail.com>
2026-06-23 01:39:46 -0500

    chore: add animation loop

--- src/hardware.ts
+++ src/hardware.ts
@@ -1,5 +1,19 @@
 // Hardware abstraction layer.
 
+import { FPS } from "./constants";
+
 export function run(tick: () => void): void {
-  tick();
+  const frameInterval = 1000 / FPS;
+  let lastTimestamp = 0;
+
+  function gameLoop(timestamp: number): void {
+    if (timestamp - lastTimestamp >= frameInterval) {
+      lastTimestamp = timestamp;
+      tick();
+    }
+
+    requestAnimationFrame(gameLoop);
+  }
+
+  requestAnimationFrame(gameLoop);
 }
