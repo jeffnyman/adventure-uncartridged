70c84b86fd0e85e433afad813632184e0148a3d5
Jeff Nyman <jeffnyman@gmail.com>
2026-06-24 03:30:01 -0500

    chore: cleanup object formatting

--- src/data/objects.ts
+++ src/data/objects.ts
@@ -59,34 +59,34 @@ const numberStates = [0, 1, 2];
 
 // prettier-ignore
 export const objectGfxNum = [
-    // State #1 : '1'
-    7,
-    0x04,                  //  X
-    0x0C,                  // XX
-    0x04,                  //  X
-    0x04,                  //  X
-    0x04,                  //  X
-    0x04,                  //  X
-    0x0E,                  // XXX
-    // State #2 : '2'
-    7,
-    0x0E,                  //  XXX
-    0x11,                  // X   X
-    0x01,                  //     X
-    0x02,                  //    X
-    0x04,                  //   X
-    0x08,                  //  X
-    0x1F,                  // XXXXX
-    // State #3 : '3'
-    7,
-    0x0E,                  //  XXX
-    0x11,                  // X   X
-    0x01,                  //     X
-    0x06,                  //   XX
-    0x01,                  //     X
-    0x11,                  // X   X
-    0x0E                   //  XXX
-]
+  // State #1 : '1'
+  7,
+  0x04,                  //  X
+  0x0C,                  // XX
+  0x04,                  //  X
+  0x04,                  //  X
+  0x04,                  //  X
+  0x04,                  //  X
+  0x0E,                  // XXX
+  // State #2 : '2'
+  7,
+  0x0E,                  //  XXX
+  0x11,                  // X   X
+  0x01,                  //     X
+  0x02,                  //    X
+  0x04,                  //   X
+  0x08,                  //  X
+  0x1F,                  // XXXXX
+  // State #3 : '3'
+  7,
+  0x0E,                  //  XXX
+  0x11,                  // X   X
+  0x01,                  //     X
+  0x06,                  //   XX
+  0x01,                  //     X
+  0x11,                  // X   X
+  0x0E                   //  XXX
+];
 
 // Object #5 : Graphic (dragon: four states — walk, bite, walk-reversed, dead)
 // This covers objects: 5, 6, 7 from ObjectId.
@@ -94,181 +94,181 @@ export const dragonStates = [0, 2, 0, 1];
 
 // prettier-ignore
 export const objectGfxDrag = [
-    // State 00 : walking
-    20,
-    0x06,                  //      XX
-    0x0F,                  //     XXXX
-    0xF3,                  // XXXX  XX
-    0xFE,                  // XXXXXXX
-    0x0E,                  //     XXX
-    0x04,                  //      X
-    0x04,                  //      X
-    0x1E,                  //    XXXX
-    0x3F,                  //   XXXXXX
-    0x7F,                  //  XXXXXXX
-    0xE3,                  // XXX   XX
-    0xC3,                  // XX    XX
-    0xC3,                  // XX    XX
-    0xC7,                  // XX   XXX
-    0xFF,                  // XXXXXXXX
-    0x3C,                  //   XXXX
-    0x08,                  //     X
-    0x8F,                  // X   XXXX
-    0xE1,                  // XXX    X
-    0x3F,                  //   XXXXXX
-    // State 01 : biting
-    22,
-    0x80,                  // X
-    0x40,                  //  X
-    0x26,                  //   X  XX
-    0x1F,                  //    XXXXX
-    0x0B,                  //     X XX
-    0x0E,                  //     XXX
-    0x1E,                  //    XXXX
-    0x24,                  //   X  X
-    0x44,                  //  X   X
-    0x8E,                  // X   XXX
-    0x1E,                  //    XXXX
-    0x3F,                  //   XXXXXX
-    0x7F,                  //  XXXXXXX
-    0x7F,                  //  XXXXXXX
-    0x7F,                  //  XXXXXXX
-    0x7F,                  //  XXXXXXX
-    0x3E,                  //   XXXXX
-    0x1C,                  //    XXX
-    0x08,                  //     X
-    0xF8,                  // XXXXX
-    0x80,                  // X
-    0xE0,                  // XXX
-    // State 02 : walking reversed
-    17,
-    0x0C,                  //     XX
-    0x0C,                  //     XX
-    0x0C,                  //     XX
-    0x0E,                  //     XXX
-    0x1B,                  //    XX X
-    0x7F,                  //  XXXXXXX
-    0xCE,                  // XX  XXX
-    0x80,                  // X
-    0xFC,                  // XXXXXX
-    0xFE,                  // XXXXXXX
-    0xFE,                  // XXXXXXX
-    0x7E,                  //  XXXXXX
-    0x78,                  //  XXXX
-    0x20,                  //   X
-    0x6E,                  //  XX XXX
-    0x42,                  //  X    X
-    0x7E                   //  XXXXXX
-]
+  // State 00 : walking
+  20,
+  0x06,                  //      XX
+  0x0F,                  //     XXXX
+  0xF3,                  // XXXX  XX
+  0xFE,                  // XXXXXXX
+  0x0E,                  //     XXX
+  0x04,                  //      X
+  0x04,                  //      X
+  0x1E,                  //    XXXX
+  0x3F,                  //   XXXXXX
+  0x7F,                  //  XXXXXXX
+  0xE3,                  // XXX   XX
+  0xC3,                  // XX    XX
+  0xC3,                  // XX    XX
+  0xC7,                  // XX   XXX
+  0xFF,                  // XXXXXXXX
+  0x3C,                  //   XXXX
+  0x08,                  //     X
+  0x8F,                  // X   XXXX
+  0xE1,                  // XXX    X
+  0x3F,                  //   XXXXXX
+  // State 01 : biting
+  22,
+  0x80,                  // X
+  0x40,                  //  X
+  0x26,                  //   X  XX
+  0x1F,                  //    XXXXX
+  0x0B,                  //     X XX
+  0x0E,                  //     XXX
+  0x1E,                  //    XXXX
+  0x24,                  //   X  X
+  0x44,                  //  X   X
+  0x8E,                  // X   XXX
+  0x1E,                  //    XXXX
+  0x3F,                  //   XXXXXX
+  0x7F,                  //  XXXXXXX
+  0x7F,                  //  XXXXXXX
+  0x7F,                  //  XXXXXXX
+  0x7F,                  //  XXXXXXX
+  0x3E,                  //   XXXXX
+  0x1C,                  //    XXX
+  0x08,                  //     X
+  0xF8,                  // XXXXX
+  0x80,                  // X
+  0xE0,                  // XXX
+  // State 02 : walking reversed
+  17,
+  0x0C,                  //     XX
+  0x0C,                  //     XX
+  0x0C,                  //     XX
+  0x0E,                  //     XXX
+  0x1B,                  //    XX X
+  0x7F,                  //  XXXXXXX
+  0xCE,                  // XX  XXX
+  0x80,                  // X
+  0xFC,                  // XXXXXX
+  0xFE,                  // XXXXXXX
+  0xFE,                  // XXXXXXX
+  0x7E,                  //  XXXXXX
+  0x78,                  //  XXXX
+  0x20,                  //   X
+  0x6E,                  //  XX XXX
+  0x42,                  //  X    X
+  0x7E                   //  XXXXXX
+];
 
 // Object #8 : State FF : Graphic
 // prettier-ignore
 export const objectGfxSword = [
-    5,
-    0x20,                  //   X
-    0x40,                  //  X
-    0xFF,                  // XXXXXXXX
-    0x40,                  //  X
-    0x20                   //   X
-]
+  5,
+  0x20,                  //   X
+  0x40,                  //  X
+  0xFF,                  // XXXXXXXX
+  0x40,                  //  X
+  0x20                   //   X
+];
 
 // Object #9: State FF : Graphic
 // prettier-ignore
 export const objectGfxBridge = [
-    24,
-    0xC3, 0xC3, 0xC3, 0xC3,   // XX    XX (×4)
-    0x42, 0x42, 0x42, 0x42,   //  X    X  (×4)
-    0x42, 0x42, 0x42, 0x42,   //  X    X  (×4)
-    0x42, 0x42, 0x42, 0x42,   //  X    X  (×4)
-    0x42, 0x42, 0x42, 0x42,   //  X    X  (×4)
-    0xC3, 0xC3, 0xC3, 0xC3    // XX    XX (×4)
-]
+  24,
+  0xC3, 0xC3, 0xC3, 0xC3,   // XX    XX (×4)
+  0x42, 0x42, 0x42, 0x42,   //  X    X  (×4)
+  0x42, 0x42, 0x42, 0x42,   //  X    X  (×4)
+  0x42, 0x42, 0x42, 0x42,   //  X    X  (×4)
+  0x42, 0x42, 0x42, 0x42,   //  X    X  (×4)
+  0xC3, 0xC3, 0xC3, 0xC3    // XX    XX (×4)
+];
 
 // Object 0x0A (10) : State FF : Graphic
 // This covers objects: 10, 11, 12 from ObjectId.
 // prettier-ignore
 export const objectGfxKey = [
-    3,
-    0x07,                  //      XXX
-    0xFD,                  // XXXXXX X
-    0xA7                   // X X  XXX
-]
+  3,
+  0x07,                  //      XXX
+  0xFD,                  // XXXXXX X
+  0xA7                   // X X  XXX
+];
 
 // Object 0x0D (13) : Graphic (bat: two states — folded wings, open wings)
 export const batStates = [0, 1];
 
 // prettier-ignore
 export const objectGfxBat = [
-    // State 03 : folded wings
-    7,
-    0x81,                  // X      X
-    0x81,                  // X      X
-    0xC3,                  // XX    XX
-    0xC3,                  // XX    XX
-    0xFF,                  // XXXXXXXX
-    0x5A,                  //  X XX X
-    0x66,                  //  XX  XX
-    // State FF : open wings
-    11,
-    0x01,                  //        X
-    0x80,                  // X
-    0x01,                  //        X
-    0x80,                  // X
-    0x3C,                  //   XXXX
-    0x5A,                  //  X XX X
-    0x66,                  //  XX  XX
-    0xC3,                  // XX    XX
-    0x81,                  // X      X
-    0x81,                  // X      X
-    0x81                   // X      X
-]
+  // State 03 : folded wings
+  7,
+  0x81,                  // X      X
+  0x81,                  // X      X
+  0xC3,                  // XX    XX
+  0xC3,                  // XX    XX
+  0xFF,                  // XXXXXXXX
+  0x5A,                  //  X XX X
+  0x66,                  //  XX  XX
+  // State FF : open wings
+  11,
+  0x01,                  //        X
+  0x80,                  // X
+  0x01,                  //        X
+  0x80,                  // X
+  0x3C,                  //   XXXX
+  0x5A,                  //  X XX X
+  0x66,                  //  XX  XX
+  0xC3,                  // XX    XX
+  0x81,                  // X      X
+  0x81,                  // X      X
+  0x81                   // X      X
+];
 
 // Object 0x0E (14) : State FF : Graphic
 // prettier-ignore
 export const objectGfxDot = [
-    1,
-    0x80                   // X
-]
+  1,
+  0x80                   // X
+];
 
 // Object 0x0F (15) : State FF : Graphic
 // prettier-ignore
 export const objectGfxChalice = [
-    9,
-    0x81,                  // X      X
-    0x81,                  // X      X
-    0xC3,                  // XX    XX
-    0x7E,                  //  XXXXXX
-    0x7E,                  //  XXXXXX
-    0x3C,                  //   XXXX
-    0x18,                  //    XX
-    0x18,                  //    XX
-    0x7E                   //  XXXXXX
-]
+  9,
+  0x81,                  // X      X
+  0x81,                  // X      X
+  0xC3,                  // XX    XX
+  0x7E,                  //  XXXXXX
+  0x7E,                  //  XXXXXX
+  0x3C,                  //   XXXX
+  0x18,                  //    XX
+  0x18,                  //    XX
+  0x7E                   //  XXXXXX
+];
 
 // Object 0x10 (16) : State FF : Graphic
 // prettier-ignore
 export const objectGfxMagnet = [
-    8,
-    0x3C,                  //   XXXX
-    0x7E,                  //  XXXXXX
-    0xE7,                  // XXX  XXX
-    0xC3,                  // XX    XX
-    0xC3,                  // XX    XX
-    0xC3,                  // XX    XX
-    0xC3,                  // XX    XX
-    0xC3                   // XX    XX
-]
+  8,
+  0x3C,                  //   XXXX
+  0x7E,                  //  XXXXXX
+  0xE7,                  // XXX  XXX
+  0xC3,                  // XX    XX
+  0xC3,                  // XX    XX
+  0xC3,                  // XX    XX
+  0xC3,                  // XX    XX
+  0xC3                   // XX    XX
+];
 
 // Surround : Graphic
 // This is not a numbered game object.
 // prettier-ignore
 export const objectGfxSurround = [
-    32,
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,   // XXXXXXXX (×8)
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,   // XXXXXXXX (×8)
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,   // XXXXXXXX (×8)
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF    // XXXXXXXX (×8)
-]
+  32,
+  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,   // XXXXXXXX (×8)
+  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,   // XXXXXXXX (×8)
+  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,   // XXXXXXXX (×8)
+  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF    // XXXXXXXX (×8)
+];
 
 // Indexed array of all game objects and their properties.
 export const objectDefs: OBJECT[] = [
